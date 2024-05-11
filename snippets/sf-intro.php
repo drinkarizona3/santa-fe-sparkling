f518be4be3d50d8e5d589cec5efb041a3f07e829
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-05-11 11:36:01 +0200

    styelsheet

--- snippets/sf-intro.php
+++ snippets/sf-intro.php
@@ -1,20 +1,23 @@
+<link rel="stylesheet" href="./css/sf-component-intro.css">
+
 <div id="sf-intro" class="d-flex flex-column justify-content-center align-items-center">
-  <div class="sf-logo">
+
+  <div class="d-flex sf-logo">
     <?php get_template_part('snippets/santa-fe-logo'); ?>
   </div>
 
   <div class="sf-fruits">
-    <?php 
+      <?php 
 
-      $flavors = SF_get_flavors();
+        $flavors = SF_get_flavors();
 
-      foreach($flavors as $flavor): ?>
+        foreach($flavors as $flavor): ?>
 
-        <div class="sf-fruit" data-flavor="<?= $flavor->post_name ?>">
-          <?php get_template_part('snippets/sf-grapefruit'); ?>
-        </div> <?php
+          <div class="sf-fruit" data-flavor="<?= $flavor->post_name ?>">
+            <?php get_template_part('snippets/sf-grapefruit'); ?>
+          </div> <?php
 
-      endforeach;
-    ?>
+        endforeach;
+      ?>
   </div>
 </div>
