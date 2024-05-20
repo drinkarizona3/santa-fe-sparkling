c314c089397e659b479fd4f79c0f63775951341a
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-05-20 22:29:52 +0200

    update

--- theme/js/sf-main.js
+++ theme/js/sf-main.js
@@ -47,7 +47,7 @@
 
       createObserver([...flavorsSection], checkIfInProductsSection, {
         root: null,
-        rootMargin: "0px 0px -50%",
+        rootMargin: "0px 0px -100%",
         threshold: 0
       });
     }
