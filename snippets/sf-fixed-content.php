51ff212752b3e47478d53c951df30f4f08268be4
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-05-20 19:39:55 +0200

    update

--- snippets/sf-fixed-content.php
+++ snippets/sf-fixed-content.php
@@ -14,7 +14,7 @@
       <?php get_template_part('snippets/sf-dots-reactangle'); ?>
     </div>
 
-    <div class="sf-dots__rectangle position-absolute">
+    <div class="sf-dots__square position-absolute">
       <?php get_template_part('snippets/sf-dots-square'); ?>
     </div>
     
