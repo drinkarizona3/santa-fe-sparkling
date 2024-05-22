1bb22433b3eb272494f78b9d5404ec5d6ecdb493
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-05-22 21:12:29 +0200

    test

--- theme/js/sf-main.js
+++ theme/js/sf-main.js
@@ -79,11 +79,11 @@
 
         const 
            target = entry.target;
-            background = document.querySelector(`#sf-background[data-sf-theme="${target.getAttribute('data-sf-theme')}"]`),
-            parallaxElements = background.querySelectorAll('[data-sf-parallax]'),
-            animateBackgroundElements = function(entry) {
-              entry.target.style.transform = `translate3d(${Math.floor(entry.intersectionRatio * 100)}%, 0, 0)`;
-            }
+            // background = document.querySelector(`#sf-background[data-sf-theme="${target.getAttribute('data-sf-theme')}"]`),
+            // parallaxElements = background.querySelectorAll('[data-sf-parallax]'),
+            // animateBackgroundElements = function(entry) {
+            //   entry.target.style.transform = `translate3d(${Math.floor(entry.intersectionRatio * 100)}%, 0, 0)`;
+            // }
 
           // if (background) {
           //   if (!parallaxElements.length) return
