7da128fc226faa0c15c2c7cd91d79e35f95be3cc
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-05-22 16:55:08 +0200

    test

--- theme/js/sf-main.js
+++ theme/js/sf-main.js
@@ -76,24 +76,26 @@
 
       function parallax(entry) {
 
-        const 
-          target = entry.target,
-          background = document.querySelector(`#sf-background[data-sf-theme="${target.getAttribute('data-sf-theme')}"]`),
-          parallaxElements = background.querySelectorAll('[data-sf-parallax]'),
-          animateBackgroundElements = function(entry) {
-            entry.target.style.transform = `translate3d(${Math.floor(entry.intersectionRatio * 100)}%, 0, 0)`;
-          };
+        // const 
+        //   // target = entry.target;
+        //   // // background = document.querySelector(`#sf-background[data-sf-theme="${target.getAttribute('data-sf-theme')}"]`),
+        //   // // parallaxElements = background.querySelectorAll('[data-sf-parallax]'),
+        //   // // animateBackgroundElements = function(entry) {
+        //   // //   entry.target.style.transform = `translate3d(${Math.floor(entry.intersectionRatio * 100)}%, 0, 0)`;
+        //   // // };
 
-          if (background) {
-            if (!parallaxElements.length) return;
+        //   // // if (background) {
+        //   // //   if (!parallaxElements.length) return;
 
-            console.log(entry.intersectionRatio);
-          }
+        //   // //   console.log(entry.intersectionRatio);
+        //   // // }
   
   
-          // function handleParallax(entry) {
-          //   if (entry.isIntersecting) animateBackgroundElements();
-          // }
+        //   // // function handleParallax(entry) {
+        //   // //   if (entry.isIntersecting) animateBackgroundElements();
+        //   // // }
+
+          console.log(entry)
 
       }
 
