238f3db627c096248f6b8c8d586af7c2ece76f9f
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-05-30 15:30:13 +0200

    remove parallax scroll

--- templates/front-page.php
+++ templates/front-page.php
@@ -55,38 +55,39 @@
       $link_target = $link['target'] ? $link['target'] : '_self';
       $column_direction = ($i % 2 === 0) ? ' flex-md-row' : ' flex-md-row-reverse'; ?>
 
-      <article id="sf-flavor-section__<?= $post->post_name ?>" class="d-flex align-items-center sf-flavor__container">
-        <div class="sf-section sf-flavor sf-sticky container-lg" data-sf-theme="<?= $post->post_name ?>">
-          <div class="row flex-column-reverse text-center text-md-left align-items-center<?= $column_direction ?>">
+      <article id="sf-flavor-section__<?= $post->post_name ?>" class="sf-section sf-flavor container-lg" data-sf-theme="<?= $post->post_name ?>">
+  
+        <div class="row flex-column-reverse text-center text-md-left align-items-center<?= $column_direction ?>">
 
-            <div class="col-md-6 sf-flavor__content">
-                <div class="inner d-flex flex-column align-items-center align-items-md-start sf-gap__large">
+          <div class="col-md-6 sf-flavor__content">
+              <div class="inner d-flex flex-column align-items-center align-items-md-start sf-gap__large">
 
-                  <h4 class="sf-bloomsbury sf-title"><?php the_title(); ?></h4>
+                <h4 class="sf-bloomsbury sf-title"><?php the_title(); ?></h4>
 
-                  <div>
-                    <?php the_content(); ?>
-                  </div>
-
-                  <?php if(!empty($link)): ?>
-                    <a href="<?= $link['url'] ?>" target="<?= $link_target ?>" class="sf-button"><?= $link['title']; ?></a>
-                  <?php endif; ?>
-
+                <div>
+                  <?php the_content(); ?>
                 </div>
-            </div>
 
-            <div class="col-md-6 sf-flavor__image">
-              <?php $flavor_img_ID = get_post_thumbnail_id($post->ID); ?>
+                <?php if(!empty($link)): ?>
+                  <a href="<?= $link['url'] ?>" target="<?= $link_target ?>" class="sf-button"><?= $link['title']; ?></a>
+                <?php endif; ?>
 
-              <figure class="sf-flavor__image-image">
-                  <?= wp_get_attachment_image( $flavor_img_ID, null, null, array(
-                    'loading' => 'lazy'
-                  )); ?>
-              </figure>
-
-            </div>
+              </div>
           </div>
+
+          <div class="col-md-6 sf-flavor__image">
+            <?php $flavor_img_ID = get_post_thumbnail_id($post->ID); ?>
+
+            <figure class="sf-flavor__image-image">
+                <?= wp_get_attachment_image( $flavor_img_ID, null, null, array(
+                  'loading' => 'lazy'
+                )); ?>
+            </figure>
+
+          </div>
+          
         </div>
+        
       </article> <?php
 
       endforeach;   wp_reset_postdata(); 
