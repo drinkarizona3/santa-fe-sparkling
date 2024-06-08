7606f2853d2c6b27837e6c6e2f1fab002adc7234
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-08 09:33:56 +0200

    update

--- templates/front-page.php
+++ templates/front-page.php
@@ -21,7 +21,7 @@
           <div class="sf-hero__background position-absolute <?= $background['position'] ?>">
             <?= wp_get_attachment_image( $background['image']['ID'], null, null, array(
               'class' => 'sf-object-fit',
-              'onload' => 'javascript: this.classList.add("az-img-loaded")'
+              'onload' => 'javascript: this.classList.add("sf-img-loaded")'
             )); ?>
           </div> <?php
         
