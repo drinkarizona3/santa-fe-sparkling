ba0254e4b46907394f5979ea37972db0d834a1fc
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-18 16:53:40 +0200

    update

--- templates/front-page.php
+++ templates/front-page.php
@@ -30,7 +30,7 @@
       endif;
     ?>
 
-    <div class="position-relative sf-hero__content" data-sf-parallax='{"speed": 1}'>
+    <div class="position-relative sf-hero__content">
       <h1 class="sf-bloomsbury text-center sf-scaling-text position-absolute">
         <span class="sf-brandname position-relative" data-text="<?= strip_tags($hero_title) ?>"><?= $hero_title ?></span>
         <span class="d-block sf-subtitle position-relative" data-text="<?= strip_tags($hero_subtitle) ?>"><?= $hero_subtitle; ?></span>
