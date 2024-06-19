7480b95031cf04053ecd2c79da13a8a49aec5a55
Gianpaolo Barone <gianpaolo@mpirecreative.com>
2024-06-19 11:48:31 +0200

    test

--- theme/js/sf-main.js
+++ theme/js/sf-main.js
@@ -110,7 +110,7 @@
 
       createObserver(flavorSections, handleIntro, {
         root: null,
-        rootMargin: "50% 0% 50% 0%",
+        rootMargin: "25% 0% 25% 0%",
         threshold: thresholds
       });
 
